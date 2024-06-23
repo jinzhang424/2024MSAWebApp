32f2ac86ee6b265d24de728bf5bd000f420b8659
Jin <ajhz011@aucklanduni.ac.nz>
2024-06-23 15:01:42 +1200

    Implemented the IMilkTeaRepository functions

---
+++ backend/Repositories/MilkTeaRepository.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Repositories {
+
+    public class MilkTeaRepository : IMilkTeaRepository {
+
+        private readonly MilkTeaContext _context;
+
+        public MilkTeaRepository(MilkTeaContext context) {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<MilkTea>> GetAllMilkTeasAsync() {
+            return await _context.MilkTea.ToListAsync();
+        }
+
+        public async Task<Models.MilkTea> GetMilkTeaByIdAsync(long id) {
+            return await _context.MilkTea.FindAsync(id);
+        }
+
+        public async Task AddMilkTeaAsync(MilkTea milkTea) {
+            _context.MilkTea.Add(milkTea);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateMilkTeaAsync(MilkTea milkTea) {
+            _context.Entry(milkTea).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteMilkTeaAsync(long id) {
+            var milkTea = await _context.MilkTea.FindAsync(id);
+            if (milkTea != null) {
+                _context.MilkTea.Remove(milkTea);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<bool> MilkTeaExistsAsync(long id) {
+            return await _context.MilkTea.AnyAsync(e => e.Id == id);
+        }
+
+        public async Task BulkMilkTeasAsync(IEnumerable<MilkTea> milkTea) {
+            await _context.MilkTea.AddRangeAsync(milkTea);
+            await _context.SaveChangesAsync();
+        }
+    } 
+}
