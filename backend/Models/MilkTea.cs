2c9ee3a4e2ecfabf016bab891fd37cffb96560ab
Jin <ajhz011@aucklanduni.ac.nz>
2024-06-23 12:35:55 +1200

    Added new Model, MilkTea and some packages

---
+++ backend/Models/MilkTea.cs
@@ -0,0 +1,8 @@
+namespace Models {
+    public class MilkTea {
+        
+        public int Id { get; set; }
+        public bool? Pearls { get; set; }
+        public string? Name { get; set; }
+    }
+}
