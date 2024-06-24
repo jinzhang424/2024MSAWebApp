9240ac59d5b9de9be80b044497740fc3a28052d7
Jin <ajhz011@aucklanduni.ac.nz>
2024-06-24 12:23:43 +1200

    Specified the localhost to 5173

--- backend/Program.cs
+++ backend/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options => {
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5000")
+            policy.WithOrigins("http://localhost:5173")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
