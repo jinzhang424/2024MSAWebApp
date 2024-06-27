8b0c4275b773e0a974aaf6b04b26cffde832a20b
Jin <ajhz011@aucklanduni.ac.nz>
2024-06-27 16:30:26 +1200

    Committing all files that changed as a result of compiling and running the program

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed697f6aba098a2821b7f4fd49ed3d78439d8ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97fcf99d739d8ae9fa81c4e99537221e6771a039")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
