829d95a35e4ac011b3d8dac700d3bcd1de3cb713
Jin <ajhz011@aucklanduni.ac.nz>
2024-06-27 18:36:40 +1200

    Added a foreground image and placed it ontop of the image slider

--- frontend/src/Routes/MainPage.tsx
+++ frontend/src/Routes/MainPage.tsx
@@ -1,10 +1,16 @@
 import NavigationBar from "../components/NavigationBar";
 import { ImageSlider } from "../components/ImageSlider";
 import { imageSlides } from "../data/imageSlides.json";
+import img1 from "../data/images/foreground.png";
 
 export default function Root() {
   return <>
-    <NavigationBar />
-    <ImageSlider data={imageSlides}/>
+  <div className="layout">
+
+      <NavigationBar />
+      <img src={img1} alt="foreground" className="img1" />
+      <ImageSlider data={imageSlides}/>
+
+  </div>
   </>
 }
