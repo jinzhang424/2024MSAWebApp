50722c74640ccfa194759ec301e6bb7e34a18295
Jin <ajhz011@aucklanduni.ac.nz>
2024-06-29 16:35:20 +1200

    Created the basic grid layout for the other elements of the main page

--- frontend/src/Routes/MainPage.tsx
+++ frontend/src/Routes/MainPage.tsx
@@ -9,13 +9,67 @@ export default function Root() {
 
       <div className="navBar"><NavigationBar /></div>
       <div className="imgSlider"><ImageSlider data={imageSlides}/></div>
+
       <div className="specials">
-        <body className="specialsText">
-          hello lorem
-        </body>
+
+        <h1 className="specialsTitle">
+          Specials
+        </h1>
+
+        <div className="special1">
+          <h1 className="special1Header">
+            Lorem Ipsum
+          </h1>
+          <body className="special1Body">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+          </body>
+        </div>
+
+        <div className="special2">
+          <h1 className="special2Header">
+            Lorem Ipsum
+          </h1>
+          <body className="special2Body">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+          </body>
+        </div>
       </div>
-      <div className="news">news</div>
-      <div className="contacts">contact info</div>
+
+      <div className="news">
+        <div className="newsPanel1">
+          panel1
+        </div>
+
+        <div className="newsPanel2">
+          panel2
+        </div>
+
+        <div className="newsPanel3">
+          panel3
+        </div>
+      </div>
+
+      <div className="contacts">
+
+        <h1 className="contactUsTitle">
+          Contact Us
+        </h1>
+
+        <div className="contactInfo">
+          <div className="address">
+            Address
+          </div>
+          <div className="phone">
+            phone
+          </div>
+          <div className="email">
+            email
+          </div>
+          <div className="socials">
+            social media
+          </div>
+        </div>
+      </div>
   </div>
   </>
 }
