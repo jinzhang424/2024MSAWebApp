5217e71379f30301bb571b1f67356aa709b324ee
Jin <ajhz011@aucklanduni.ac.nz>
2024-07-04 13:10:24 +1200

    Made the navigation bar resize appropriately and change to using a open/closeable sidebar menu when the width gets too small

--- frontend/src/Components/NavigationBar.tsx
+++ frontend/src/Components/NavigationBar.tsx
@@ -1,26 +1,46 @@
 import './NavigationBar.css';
 import imgLogo from "../data/images/logo.svg";
 
-const NavigationBar = () => {
-  return <>
-    <header className="header">
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
-      <a href="/">
-        <img 
-          src={imgLogo} 
-          alt="Bobuzz logo" 
-          className="logo"
-        />
-      </a>
-      
-      <nav className="navBar">
-        <a href="/">Home</a>
-        <a href="/about-us">About Us</a>
-        <a href="/menu">Menu</a>
-        <a href="order/">Order</a>
+const NavigationBar = () => {
+  return (
+
+      <nav className="navBarContainer">
+
+          <a href="/">
+            <img 
+              src={imgLogo} 
+              alt="Bobuzz logo" 
+              className="logo"
+            />
+          </a>
+
+        <input type="checkbox" id="sideBarActive"/>
+
+        <label htmlFor="sideBarActive" id="overlay"></label>
+
+        <label htmlFor="sideBarActive" className="openSideBarButton">
+          <MenuIcon fontSize="large" sx={{ color: "white", filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.4))" }} />
+        </label>
+
+        <div className="linksContainer">
+          <label htmlFor="sideBarActive" className="closeSideBarButton">
+            <CloseIcon fontSize="large" sx={{ color: "white" }} />
+          </label>
+          
+          <nav className="links">
+            <a href="/" className="navBarLink">Home</a>
+            <a href="/about-us" className="navBarLink">About Us</a>
+            <a href="/menu" className="navBarLink">Menu</a>
+            <a href="order/" className="navBarLink">Order</a>
+          </nav>
+        </div>
+
       </nav>
-    </header>
-  </>
+
+  )
 }
 
 export default NavigationBar
