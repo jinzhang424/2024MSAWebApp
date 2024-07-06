9b2c327aba1e603a1a462486dbbc7a493ce229a7
Jin <ajhz011@aucklanduni.ac.nz>
2024-07-06 12:51:31 +1200

    Changed the names of SpecialPanel to NewsPanel

---
+++ frontend/src/Components/NewsPanel.tsx
@@ -0,0 +1,30 @@
+import "./NewsPanel.css"
+
+import { Typography } from "@mui/material";
+ 
+interface NewsPanelProps {
+    imageUrl: string;
+    imageAlt: string;
+    imageTitle: string;
+    text: string;
+}
+
+  export const NewsPanel = ({ imageUrl, imageAlt, imageTitle, text }: NewsPanelProps) => {
+
+    return (
+        <div className="NewsPanelContainer">
+
+            <img src={imageUrl} alt={imageAlt} className="NewsPanelImg"/>
+
+            <div className="NewsPanelText">
+                <h1 className="NewsPanelHeader">
+                    {imageTitle}
+                </h1>
+
+                <Typography>
+                    {text}
+                </Typography>
+            </div>
+        </div>
+    )
+}
