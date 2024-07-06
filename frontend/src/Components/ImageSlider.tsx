ef6cf05c0e3d807b526356716cac24d9d8506d97
Jin <ajhz011@aucklanduni.ac.nz>
2024-07-06 12:51:54 +1200

    Added the specials section to the main page

--- frontend/src/Components/ImageSlider.tsx
+++ frontend/src/Components/ImageSlider.tsx
@@ -1,6 +1,6 @@
+import { useState, useEffect } from "react";
 import "./NavigationBar.css";
 import "./ImageSlider.css";
-import { useState } from "react";
 
 import Fab from '@mui/material/Fab';
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
@@ -17,6 +17,7 @@ interface ImageSliderProps {
 
 export const ImageSlider = ({ data } : ImageSliderProps) => {
 
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     const [slide, setSlide] = useState(0);
     
     const nextSlide = () => {
@@ -27,7 +28,32 @@ export const ImageSlider = ({ data } : ImageSliderProps) => {
         setSlide(slide === 0 ? data.length - 1 : slide - 1);
     }
 
+    useEffect(() => {
+        const handleResize = () => {
+          setWindowWidth(window.innerWidth);
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+    
+      
+      const getFabSize = () => {
+        if (windowWidth > 900) return 'large'; // Large size for wide screens
+        if (windowWidth > 700) return 'medium'; // Medium size for tablets
+        return 'small'; // Small size for mobile devices
+      };
 
+      const FabSize = getFabSize();
+
+      // Floating action button sizes
+      const FabStyles = {
+        large: { width: '70px', height: '70px', fontSize: '30px' },
+        medium: { width: '50px', height: '50px', fontSize: '25px' },
+        small: { width: '40px', height: '40px', fontSize: '20px' },
+      };
 
     return (
     
@@ -42,13 +68,16 @@ export const ImageSlider = ({ data } : ImageSliderProps) => {
                 />
             })}
 
-            <Fab aria-label="ArrowLeftIcon" onClick={prevSlide} style={{zIndex: 2}} className="arrow leftArrow">
-                <ArrowLeftIcon />
-            </Fab>
+            <div className="Fabs">
+                <Fab aria-label="ArrowLeftIcon" onClick={prevSlide} style={{ ...FabStyles[FabSize] }} className="leftArrow customFab">
+                    <ArrowLeftIcon />
+                </Fab>
 
-            <Fab aria-label="ArrowRightIcon" onClick={nextSlide} style={{zIndex: 2}} className="arrow rightArrow">
-                <ArrowRightIcon />
-            </Fab>
+                <Fab aria-label="ArrowRightIcon" onClick={nextSlide} style={{ ...FabStyles[FabSize] }} className="rightArrow customFab">
+                    <ArrowRightIcon />
+                </Fab>
+            </div>
+
             
             <span className="indicatorContainer">
                 {data.map((_, index) => {
--- frontend/src/Routes/MainPage.tsx
+++ frontend/src/Routes/MainPage.tsx
@@ -1,15 +1,21 @@
 import './MainPage.css';
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import NavigationBar from "../components/NavigationBar";
-import { SpecialsPanel } from "../components/SpecialsPanel"
+import { NewsPanel } from "../components/NewsPanel"
 import { ImageSlider } from "../components/ImageSlider";
 import { imageSlides } from "../data/imageSlides.json";
-import { Typography } from "@mui/material";
+import Typography from '@mui/material/Typography';
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 import greenTeaLatte from "../data/specialsImages/greenTea-latte.jpg";
-import chocolateOreaMilkTea from "../data/specialsImages/chocolateOreaMilkTea.jpg";
+import chocolateOreoMilkTea from "../data/specialsImages/chocolateOreaMilkTea.jpg";
+
+let special1Title: string = "Green Tea Latte";
+let special1Text: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex sunt vero fuga, itaque sapiente iusto soluta necessitatibus illum consectetur suscipit incidunt qui odit nemo, quisquam, in quasi dolores nihil?"
+let special2Title: string = "Oreo MilkShake";
+let special2Text: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex sunt vero fuga, itaque sapiente iusto soluta necessitatibus illum consectetur suscipit incidunt qui odit nemo, quisquam, in quasi dolores nihil?"
 
 import upcomingMacarons from "../data/images/upcomingMacarons.jpg";
 import upcomingMilkshakes from "../data/images/upcomingMilkshakes.jpg";
@@ -57,6 +63,44 @@ export default function Root() {
     zIndex: navBarZIndex,
   };
 
+  const theme = createTheme();
+
+  theme.typography.h2 = {
+    fontSize: '2.6rem',
+    '@media (max-width:600px)': {
+      fontSize: '2.2rem',
+    },
+    '@media (max-width:400px)': {
+      fontSize: '2.1rem',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '3.4rem',
+    },
+  };
+
+  theme.typography.h3 = {
+    fontSize: '2.4rem',
+    '@media (max-width:600px)': {
+      fontSize: '1.8rem',
+    },
+    '@media (max-width:450px)': {
+      fontSize: '1.5rem',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '3rem',
+    },
+  };
+  
+  theme.typography.body1 = {
+    '@media (max-width:450px)': {
+      fontSize: '0.8rem',
+    },
+
+    '@media (min-width:1000px)': {
+      fontSize: '1.3rem',
+    },
+  }
+
   return (
     <div className="layout">
 
@@ -64,7 +108,51 @@ export default function Root() {
           <div className="navBar" style={navBarStyle}><NavigationBar /></div>
           <div className="imgSlider"><ImageSlider data={imageSlides}/></div>
         </div>
+        
+        <div className="specialsSection">
 
+          <ThemeProvider theme={theme}>
+            <Typography variant="h2" className="specials">
+              Specials
+            </Typography>
+          </ThemeProvider>
+
+          <div className="special1Container">
+            <div className="special1TextContainter">
+              <ThemeProvider theme={theme}>
+                <Typography variant="h3" className="special1Title">
+                  {special1Title}
+                </Typography>
+                <Typography variant="body1" className="special1Body">
+                  {special1Text}
+                </Typography>
+              </ThemeProvider>
+            </div>
+            <img src={greenTeaLatte} alt="green tea latte special" className="special1Img" />
+          </div>
+
+          <div className="special2Container">
+            <div className="special2TextContainer">
+              <ThemeProvider theme={theme}>
+                <Typography variant="h3" className="special2Title">
+                  {special2Title}
+                </Typography>
+                <Typography variant="body1" className="special2Body">
+                  {special1Text}
+                </Typography>
+              </ThemeProvider>
+            </div>
+            <img src={chocolateOreoMilkTea} alt="chocolate milk tea speical" className="special2Img" />
+          </div>
+        </div>
+
+        <div>
+          news
+        </div>
+
+        <div>
+          contacts
+        </div>
     </div>
   );
 };
